67fbfe0179660071564cf986ce9809367f046142
Angel <angel@Angels-MacBook-Air.local>
2023-05-24 17:57:38 -0400

    updated home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,38 @@
-//import React from "react";
+import React, { useState } from "react";
 
 export default function Home() {
-//   fetch(
-//     `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}`
-//   );
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}`
+    );
+    
+  const [input, setInput] = useState({
+    userInput: "",
+  });
+
+  const [search, setSearch] = useState(false);
+
+  function handleTextChange(event) {
+    event.preventDefault();
+
+    setInput((input.userInput = event.target.value));
+    console.log(input.userInput);
+  }
+
+  function handleSearch() {
+    setSearch(!search);
+  }
+
+  return (
+    <div className="home-container">
+      <form>
+        <label>Search</label>
+        <input
+          type="text"
+          value={input.userInput}
+          onChange={(event) => handleTextChange(event)}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </form>
+    </div>
+  );
 }
