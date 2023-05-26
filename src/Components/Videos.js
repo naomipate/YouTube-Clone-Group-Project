108f201c32558a794bd74f312ef83fad90bbd328
Naomi-Pate <patenaomi99@gmail.com>
2023-05-26 15:15:21 -0400

    updated Videos with StaticVideo.json and made StaticVideo.json

--- src/Components/Videos.js
+++ src/Components/Videos.js
@@ -1,11 +1,17 @@
 import { useParams } from "react-router-dom";
-import staticData from "./StaticData.json"
+import staticData from "./StaticData.json";
+// import staticVideo from "./StaticVideo.json";
 export default function Videos() {
-console.log(staticData)
+  // const VIDEO_URL = `https://youtube.googleapis.com/youtube/v3/videos?part=player&id=${id}&key=${process.env.REACT_APP_API_KEY}`;
+  console.log(staticData);
   const { id } = useParams();
-  let result = staticData.items.filter((element)=>element.id.videoId === id)
-  console.log(result)
-//   let obj = {...result[0].id}
-//   console.log(obj.videoId)
+  let result = staticData.items.filter((element) => element.id.videoId === id);
+  console.log(result);
+  // let obj = {...result[0].id}
+  // console.log(obj.videoId)
+
+  // let videoResult = staticVideo.items.map(
+  //   (element) => element.player.embedHtml
+  // );
   return <div>this is video</div>;
 }
