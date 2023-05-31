20dd845cf69bb80d22fab0a02c11428d12060685
Naomi-Pate <patenaomi99@gmail.com>
2023-05-31 13:55:42 -0400

    cleaned up and updated main

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import staticData from "./StaticData.json";
+//import staticData from "./StaticData.json";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
@@ -8,33 +8,25 @@ export default function Home() {
   const [input, setInput] = useState("");
   const [searchState, setSearchState] = useState(false);
 
-  const [searched, setSearched] = useState(false);
-
   function handleInput(event) {
     setInput(event.target.value);
   }
-  console.log(input);
 
   function handleSubmit(event) {
     event.preventDefault();
-    setSearched(true);
-    // setTitle(input);
-    // setInput("");
-
-    search(title);
+    if (input !== "") {
+      setSearchState(true);
+      search(input);
+      setInput("");
+    }
   }
 
-  function search(title) {
-    if (title === "") {
-      return "Please search something.";
-    } else {
-      // const BASE_URL = `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}&q=${title}&type=video&part=snippet`;
-      //   axios
-      //     .get(BASE_URL)
-      //     .then((response) => setData(response.json()))
-      //     .then((responseData) => setData(responseData.data))
-      //     .catch((error) => return(error));
-    }
+  function search() {
+    // const BASE_URL = `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}&q=${input}&type=video&part=snippet`;
+    //   axios
+    //     .get(BASE_URL)
+    //     .then((response) => setSearchData(response.data))
+    //     .catch((error) => console.log(error));
   }
 
   return (
@@ -58,21 +50,25 @@ export default function Home() {
       </div>
       <div className="container my-5">
         <div className="row text-center m-4">
-          {staticData.items.map((element) => {
-            return (
-              <Link
-                to={`/videos/${element.id.videoId}`}
-                className="col-sm-6 p-2"
-              >
-                <img
-                  src={element.snippet.thumbnails.medium.url}
-                  className="img-thumbnail"
-                  alt=""
-                />
-                <h5 className="text-wrap m-5">{element.snippet.title}</h5>
-              </Link>
-            );
-          })}
+          {searchState ? (
+            searchData.items.map((element) => {
+              return (
+                <Link
+                  to={`/videos/${element.id.videoId}`}
+                  className="col-sm-6 p-2"
+                >
+                  <img
+                    src={element.snippet.thumbnails.medium.url}
+                    className="img-thumbnail"
+                    alt=""
+                  />
+                  <h5 className="text-wrap m-5">{element.snippet.title}</h5>
+                </Link>
+              );
+            })
+          ) : (
+            <p>Oh No! Search for a video 😎</p>
+          )}
         </div>
       </div>
     </>
