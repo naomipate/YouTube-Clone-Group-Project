606d9058ee00175d4d4784e5a8e3fc7feebf4ffc
Naomi-Pate <patenaomi99@gmail.com>
2023-05-31 16:35:41 -0400

    Video fetch working, YouTube API will post multiple times on it's own, don't worry about it

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 //import staticData from "./StaticData.json";
 import { Link } from "react-router-dom";
 import axios from "axios";
